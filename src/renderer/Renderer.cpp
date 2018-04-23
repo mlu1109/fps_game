82fd9d26e4e819389586341046953d06b3daa95b
Matti Lundgren <mattilundgren@gmail.com>
2018-04-23 03:00:45 +0200

    Keep track of bound shader. Add fallback texture. Wireframe rendering

--- src/renderer/Renderer.cpp
+++ src/renderer/Renderer.cpp
@@ -13,31 +13,41 @@ void Renderer::pre()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-void Renderer::render(const Drawable &object)
+void Renderer::render(const std::shared_ptr<Shader> &shader, const std::shared_ptr<VertexArray> &vertexArray, const std::shared_ptr<Texture> &texture, const glm::mat4 &modelWorld)
 {
-    const auto& shader = object.getShader();
-    const auto& va = object.getVertexArray();
-    const auto& tex = object.getTexture();
-    const auto& modelWorld = object.getModelWorld();
-
-    shader->bind();
-    glm::mat4 worldView = m_camera->getWorldView();
-    glUniformMatrix4fv(UNIFORM_WORLD_TO_VIEW, 1, GL_FALSE, &worldView[0][0]);
-    glm::mat4 viewScreen = m_camera->getViewScreen();
-    glUniformMatrix4fv(UNIFORM_VIEW_TO_SCREEN, 1, GL_FALSE, &viewScreen[0][0]);
-    va->bind();
-    if (tex)
-        tex->bind();
+    if (m_boundShader != shader)
+    {
+        shader->bind();
+        m_boundShader = shader;
+        const glm::mat4 &worldView = m_camera->getWorldView();
+        const glm::mat4 &viewScreen = m_camera->getViewScreen();
+        glUniformMatrix4fv(UNIFORM_WORLD_TO_VIEW, 1, GL_FALSE, &worldView[0][0]);
+        glUniformMatrix4fv(UNIFORM_VIEW_TO_SCREEN, 1, GL_FALSE, &viewScreen[0][0]);
+    }
+    
+    vertexArray->bind();
     glUniformMatrix4fv(UNIFORM_MODEL_TO_WORLD, 1, GL_FALSE, &modelWorld[0][0]);
 
-    GLsizei indexCount = va->getIndexCount();
+    if (texture)
+        texture->bind();
+    else if (m_textureMissing)
+        m_textureMissing->bind();
+
+    GLsizei indexCount = vertexArray->getIndexCount();
     if (indexCount > 0)
-        glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+        glDrawElements(m_primitive, indexCount, GL_UNSIGNED_INT, 0);
     else
-        glDrawArrays(GL_TRIANGLES, 0, va->getVertexCount());
+        glDrawArrays(m_primitive, 0, vertexArray->getVertexCount());
+    
+}
+
+void Renderer::render(const Drawable &object)
+{
+    render(object.getShader(), object.getVertexArray(), object.getTexture(), object.getModelWorld());
 }
 
 void Renderer::post()
 {
+    m_boundShader = nullptr;
     glfwSwapBuffers(m_window);
 }
--- src/renderer/Renderer.hpp
+++ src/renderer/Renderer.hpp
@@ -12,10 +12,22 @@ class Renderer
     GLFWwindow *m_window;
     Camera *m_camera;
 
+    GLenum m_primitive = GL_TRIANGLES;
+    std::shared_ptr<Texture> m_textureMissing;
+    // Bound objects
+    std::shared_ptr<Shader> m_boundShader;
+
   public:
     Renderer(GLFWwindow *window, Camera *camera);
 
+    void setTextureMissing(const std::shared_ptr<Texture> &tex) { m_textureMissing = tex; }
+    void setPrimitive(GLenum mode) { m_primitive = mode; }
+
+    void enableWireframe() const { glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); }
+    void disableWireframe() const { glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); }
+
     void pre();
+    void render(const std::shared_ptr<Shader> &, const std::shared_ptr<VertexArray> &, const std::shared_ptr<Texture> &, const glm::mat4 &modelWorld); 
     void render(const Drawable &);
     void post();
 };
